25dc6b792bc0255f8afb1f4a52a91d25e3316859
sumit <sumit@localhost.localdomain>
2022-12-14 07:35:31 -0600

    Include go original database sql

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,6 @@
 go build && \
 git add . && \
-git commit -m "Include go original database sql" && \
+git commit -m "Include go original database sql" 
 git tag -a v0.0.21 $(git log --format="%H" -n 1) -m "database sql" && \
 git push && \
 git push origin v0.0.21
